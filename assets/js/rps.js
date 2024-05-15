de2affbc0746536f36a4ab6cdc0a045ae6f3f9fa
Ryan Heneghan <92156060+ryan-heneghan@users.noreply.github.com>
2024-05-15 01:05:27 +0100

    fixed small issue with displaying winner

--- assets/js/rps.js
+++ assets/js/rps.js
@@ -1,20 +1,30 @@
 function loadRandomImage(pick) {
+	// Computer picks their rps
 	var randomNumber = Math.floor(Math.random() * 3) + 1;
 	var imagePath = 'images/rps/' + randomNumber + '.png';
+	var result = 'try some rps!';
 	document.getElementById('randomImage').src = imagePath;
-	var result = ' ';
 
-	if (pick === computerChoice) {
+	// Decide who wins
+	if (pick == randomNumber)
+	{
 		result = 'It\'s a tie!';
 	}
-	else if ((pick === 1 && randomNumber === 3) ||
-		(pick === 2 && randomNumber === 1) ||
-		(pick === 3 && randomNumber === 2)) {
+	else if ((pick == 1 && randomNumber == 3) ||
+		(pick == 2 && randomNumber == 1) ||
+		(pick == 3 && randomNumber == 2))
+	{
 		result = 'You win!';
 	}
-	else {
+	else
+	{
 		result = 'You lose!';
 	}
 
 	document.getElementById('rpsResult').textContent = result;
+
+	// The 0-2er asking for a bo5
+	var wins = 0;
+	var loses = 0;
+	var draws = 0;
 }
