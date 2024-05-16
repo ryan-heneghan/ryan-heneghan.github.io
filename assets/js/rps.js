82c8998630c3974011d1faf9f001c1928ffc521b
Ryan Heneghan <92156060+ryan-heneghan@users.noreply.github.com>
2024-05-16 23:41:37 +0100

    rps changes
    better formatting + win:lose counter + text changes

--- assets/js/rps.js
+++ assets/js/rps.js
@@ -1,3 +1,7 @@
+var wins = 0;
+var loses = 0;
+var draws = 0;
+
 function loadRandomImage(pick) {
 	// Computer picks their rps
 	var randomNumber = Math.floor(Math.random() * 3) + 1;
@@ -9,22 +13,24 @@ function loadRandomImage(pick) {
 	if (pick == randomNumber)
 	{
 		result = 'It\'s a tie!';
+		draws++;
 	}
 	else if ((pick == 1 && randomNumber == 3) ||
 		(pick == 2 && randomNumber == 1) ||
 		(pick == 3 && randomNumber == 2))
 	{
 		result = 'You win!';
+		wins++;
 	}
 	else
 	{
 		result = 'You lose!';
+		loses++;
 	}
 
 	document.getElementById('rpsResult').textContent = result;
 
-	// The 0-2er asking for a bo5
-	var wins = 0;
-	var loses = 0;
-	var draws = 0;
+	var scores = "Wins: " + wins + " | Loses: " + loses + " | Draws: " + draws;
+	document.getElementById('scoreText').textContent = scores;
+
 }
